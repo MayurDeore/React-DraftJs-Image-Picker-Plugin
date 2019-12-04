146f43695e499991a59f73d473916fcc3ea9ef96
Mayur Deore <deoresmayur@gmail.com>
2019-12-05 00:22:23 +0530

    Updates

---
+++ precache-manifest.2158d7e61e493bc38a338518f7801eef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07f77fae6cf203eb9435fc1b8774d83",
+    "url": "/react-draftjs-image-picker/index.html"
+  },
+  {
+    "revision": "6a600bf761d729b9c3c9",
+    "url": "/react-draftjs-image-picker/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "b15d078e06b20db2b331",
+    "url": "/react-draftjs-image-picker/static/css/main.b367fed8.chunk.css"
+  },
+  {
+    "revision": "6a600bf761d729b9c3c9",
+    "url": "/react-draftjs-image-picker/static/js/2.b84e8c70.chunk.js"
+  },
+  {
+    "revision": "b15d078e06b20db2b331",
+    "url": "/react-draftjs-image-picker/static/js/main.4c639611.chunk.js"
+  },
+  {
+    "revision": "f4d2cafba87341f19a8d",
+    "url": "/react-draftjs-image-picker/static/js/runtime-main.14c9a16b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-draftjs-image-picker/static/media/logo.25bf045c.svg"
+  }
+]);
